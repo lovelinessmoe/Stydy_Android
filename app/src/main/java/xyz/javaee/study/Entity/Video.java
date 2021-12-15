647a24f8d2e0aabb836f5408228c799b8744d9c7
lovelinessmoe <lovelinessmoe@gmail.com>
2021-12-15 11:00:21 +0800

    删除顶栏，video未完成

--- app/src/main/java/xyz/javaee/study/Entity/Video.java
+++ app/src/main/java/xyz/javaee/study/Entity/Video.java
@@ -1,5 +1,65 @@
 package xyz.javaee.study.Entity;
 
 public class Video {
+    //层级
+    private int level;
+    //id
+    private int id;
+    //父id
+    private int p_id;
+    //章节名字
+    private String name;
+    //视频URL
     private String URL;
+
+    public Video() {
+    }
+
+    public Video(int level, int id, int p_id, String name, String URL) {
+        this.level = level;
+        this.id = id;
+        this.p_id = p_id;
+        this.name = name;
+        this.URL = URL;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getP_id() {
+        return p_id;
+    }
+
+    public void setP_id(int p_id) {
+        this.p_id = p_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
 }
--- app/src/main/java/xyz/javaee/study/HomeFragment.java
+++ app/src/main/java/xyz/javaee/study/HomeFragment.java
@@ -1,5 +1,6 @@
 package xyz.javaee.study;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,18 +8,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
+    private View mRootView;
 
-    public HomeFragment(){
+    public HomeFragment() {
 
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        mRootView = inflater.inflate(R.layout.fragment_home, container, false);
+        return mRootView;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
+        Button button = mRootView.findViewById(R.id.Button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction("xyz.javaee.videoPlay");
+                startActivity(intent);
+            }
+        });
+    }
 }

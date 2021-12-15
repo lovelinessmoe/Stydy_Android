5dce3d570ffb05446fd57edd75c043501ac8c0fc
ciwei <317523946@qq.com>
2021-12-15 09:30:06 +0800

    study_home_version1

--- app/src/main/java/xyz/javaee/study/LearnFragment.java
+++ app/src/main/java/xyz/javaee/study/LearnFragment.java
@@ -1,19 +1,264 @@
 package xyz.javaee.study;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.io.File;
+
+import javax.security.auth.callback.Callback;
 
 
-public class LearnFragment extends Fragment{
+public class LearnFragment extends Fragment {
+
+    private final String TAG = "main";
+    private EditText et_path;
+    private SurfaceView sv;
+    private Button btn_play, btn_pause, btn_replay, btn_stop;
+    private MediaPlayer mediaPlayer;
+    private SeekBar seekBar;
+    private int currentPosition = 0;
+    private boolean isPlaying;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return  inflater.inflate(R.layout.fragment_learn, container, false);
+        seekBar = getActivity().findViewById(R.id.seekBar);
+        sv = getActivity().findViewById(R.id.sv);
+        et_path = getActivity().findViewById(R.id.et_path);
+
+        btn_play = getActivity().findViewById(R.id.btn_play);
+        btn_pause = getActivity().findViewById(R.id.btn_pause);
+        btn_replay = getActivity().findViewById(R.id.btn_replay);
+        btn_stop = getActivity().findViewById(R.id.btn_stop);
+
+        btn_play.setOnClickListener(click);
+        btn_pause.setOnClickListener(click);
+        btn_replay.setOnClickListener(click);
+        btn_stop.setOnClickListener(click);
+
+        // 为SurfaceHolder添加回调
+        sv.getHolder().addCallback((SurfaceHolder.Callback) callback);
+
+        // 为进度条添加进度更改事件
+        seekBar.setOnSeekBarChangeListener(change);
+        return inflater.inflate(R.layout.fragment_learn, container, false);
+    }
+
+    private final Callback callback = new Callback() {
+        // SurfaceHolder被修改的时候回调
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            Log.i(TAG, "SurfaceHolder 被销毁");
+            // 销毁SurfaceHolder的时候记录当前的播放位置并停止播放
+            if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                currentPosition = mediaPlayer.getCurrentPosition();
+                mediaPlayer.stop();
+            }
+        }
+
+        public void surfaceCreated(SurfaceHolder holder) {
+            Log.i(TAG, "SurfaceHolder 被创建");
+            if (currentPosition > 0) {
+                // 创建SurfaceHolder的时候，如果存在上次播放的位置，则按照上次播放位置进行播放
+                play(currentPosition);
+                currentPosition = 0;
+            }
+        }
+
+        public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                                   int height) {
+            Log.i(TAG, "SurfaceHolder 大小被改变");
+        }
+
+    };
+
+    private final SeekBar.OnSeekBarChangeListener change = new SeekBar.OnSeekBarChangeListener() {
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // 当进度条停止修改的时候触发
+            // 取得当前进度条的刻度
+            int progress = seekBar.getProgress();
+            if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                // 设置当前播放的位置
+                mediaPlayer.seekTo(progress);
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress,
+                                      boolean fromUser) {
+
+        }
+    };
+
+    private final View.OnClickListener click = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.btn_play:
+                    play(0);
+                    break;
+                case R.id.btn_pause:
+                    pause();
+                    break;
+                case R.id.btn_replay:
+                    replay();
+                    break;
+                case R.id.btn_stop:
+                    stop();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+
+    /*
+     * 停止播放
+     */
+    protected void stop() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+            btn_play.setEnabled(true);
+            isPlaying = false;
+        }
+    }
+
+    /**
+     * 开始播放
+     *
+     * @param msec 播放初始位置
+     */
+    protected void play(final int msec) {
+        // 获取视频文件地址
+        String path = et_path.getText().toString().trim();
+        File file = new File(path);
+        if (!file.exists()) {
+            Toast.makeText(getActivity(), "视频文件路径错误", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            // 设置播放的视频源
+            mediaPlayer.setDataSource(file.getAbsolutePath());
+            // 设置显示视频的SurfaceHolder
+            mediaPlayer.setDisplay(sv.getHolder());
+            Log.i(TAG, "开始装载");
+            mediaPlayer.prepareAsync();
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    Log.i(TAG, "装载完成");
+                    mediaPlayer.start();
+                    // 按照初始位置播放
+                    mediaPlayer.seekTo(msec);
+                    // 设置进度条的最大进度为视频流的最大播放时长
+                    seekBar.setMax(mediaPlayer.getDuration());
+                    // 开始线程，更新进度条的刻度
+                    new Thread() {
+
+                        @Override
+                        public void run() {
+                            try {
+                                isPlaying = true;
+                                while (isPlaying) {
+                                    int current = mediaPlayer
+                                            .getCurrentPosition();
+                                    seekBar.setProgress(current);
+
+                                    sleep(500);
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }.start();
+
+                    btn_play.setEnabled(false);
+                }
+            });
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    // 在播放完毕被回调
+                    btn_play.setEnabled(true);
+                }
+            });
+
+            mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+
+                @Override
+                public boolean onError(MediaPlayer mp, int what, int extra) {
+                    // 发生错误重新播放
+                    play(0);
+                    isPlaying = false;
+                    return false;
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 重新开始播放
+     */
+    protected void replay() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.seekTo(0);
+            Toast.makeText(getActivity(), "重新播放", Toast.LENGTH_SHORT).show();
+            btn_pause.setText("暂停");
+            return;
+        }
+        isPlaying = false;
+        play(0);
+
+
+    }
+
+    /**
+     * 暂停或继续
+     */
+    protected void pause() {
+        if (btn_pause.getText().toString().trim().equals("继续")) {
+            btn_pause.setText("暂停");
+            mediaPlayer.start();
+            Toast.makeText(getActivity(), "继续播放", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            btn_pause.setText("继续");
+            Toast.makeText(getActivity(), "暂停播放", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
